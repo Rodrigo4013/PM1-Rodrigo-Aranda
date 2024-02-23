2801714dca3d31e41320f69fb4d311a101619a07
Rodrigo Aranda <ing.rodrigo.aranda@gmail.com>
2024-02-22 23:24:03 -0600

    lecture 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 class Activity {
-    constructor(id,tittle, description, imgUrl){
+    constructor(id,title, description, imgUrl){
         this.id = id;
-        this.tittle = tittle;
+        this.title = title;
         this.description = description;
         this.imgUrl = imgUrl;
     }
@@ -36,115 +36,81 @@ const toHTML = (activity) =>{
     const { id, title, description, imgUrl } = activity;
   
     // Crear elementos HTML
-    const newDiv = document.createElement("div");
-    const newTittle = document.createElement("h3");
-    const newDescrip = document.createElement("p");
-    const newImg = document.createElement("img");
-    const deleteButton = document.createElement("button");
+    const titleHtml = document.createElement("h3");
+    const descriptionHtml = document.createElement("p");
+    const imgHtml = document.createElement("img");
+    
   
     // Asignar valores a las propiedades de los elementos
-    newTittle.innerHTML = title;
-    newDescrip.innerHTML = description;
-    newImg.src = imgUrl;
-    newImg.alt = title; // Asignar el título como texto alternativo de la imagen
-    deleteButton.innerHTML = "Delete";
-  
-    // Agregar clases CSS a los elementos
-    newDiv.classList.add("card_item");
-    newTittle.classList.add("title");
-    newDescrip.classList.add("description");
-    newImg.classList.add("card_img");
-    deleteButton.classList.add("btn");
-    deleteButton.id = id;
-  
-    // Agregar un evento de clic al botón de eliminar
-    deleteButton.addEventListener("click", () => {
-      handleDeleteActivity(id);
-    });
-  
-    // Appendear los elementos al nuevo <div>
-    newDiv.appendChild(newImg);
-    newDiv.appendChild(newTittle);
-    newDiv.appendChild(newDescrip);
-    newDiv.appendChild(deleteButton);
-  
-    // Asignar al <div> la clase CSS correspondiente
-    newDiv.classList.add("card_item");
-  
-    // Retornar el <div> finalizado con todos los elementos
-    return newDiv;
+    titleHtml.innerHTML = title;
+    descriptionHtml.innerHTML = description;
+    imgHtml.src = imgUrl;
+
+
+    const card = document.createElement('div')
+
+
+    //se necesita crear una clase en css
+    card.className = 'card'
+    card.id = id;
+
+
+    card.appendChild(titleHtml)
+    card.appendChild(descriptionHtml)
+    card.appendChild(imgHtml)
+
+    return card
   }
   
-  const updateCardContainer = () => {
-    // Seleccionar el contenedor
-    const cardContainer = document.getElementById("cardContainer");
-  
-    // Vaciar el contenido actual del contenedor
-    cardContainer.innerHTML = "";
-  
-    // Obtener el listado completo de actividades
-    const activities = repository.getAllActivities();
-  
-    // Mapear el listado de actividades para convertirlos en elementos HTML
-    const activityElements = activities.map((activity) => toHTML(activity));
-  
-    // Appendear todos los elementos HTML al contenedor
-    cardContainer.append(...activityElements);
-  }
-  
-  // Llamada inicial para actualizar el contenedor con las actividades existentes
-  updateCardContainer();
-  
-  const handlerCreateActivity = (event) => {
-    // Prevenir el comportamiento por defecto del formulario
+
+  const builActivities = () => {
+    const cardsContainer = document.getElementById("cardsContainer")
+    cardsContainer.innerHTML = ""
+
+    const allActivities = repository.getAllActivities()
+
+    const htlmActivities = allActivities.map((activity)=>
+        toHTML(activity)
+    )
+
+    htlmActivities.forEach((activityHTML)=>
+        cardsContainer.appendChild(activityHTML)
+    ) 
+    
+}
+
+const handleClick = (event) => {
     event.preventDefault();
-  
-    // Seleccionar los inputs de title, description e imgUrl
-    const titleInput = document.getElementById("titleInput");
-    const descriptionInput = document.getElementById("descriptionInput");
-    const urlInput = document.getElementById("urlInput");
-  
-    // Tomar los valores ingresados en los inputs y guardarlos en variables
-    const title = titleInput.value;
-    const description = descriptionInput.value;
-    const imgUrl = urlInput.value;
-  
-    // Validar que los valores estén completos
-    if (!title || !description || !imgUrl) {
-      alert("Por favor, complete todos los campos.");
-      return; // Cortar el proceso si hay datos incompletos
+    const title = document.getElementById('title')
+    const description = document.getElementById('description')
+    const imgUrl = document.getElementById('imgUrl')
+
+    let titleValue = title.value
+    let descriptionValue = description.value
+    let imgUrlValue = imgUrl.value
+
+    //validation
+    if(!titleValue || !descriptionValue || !imgUrlValue){
+        alert("llena todos los campos")
+        return;
     }
-  
-    // Llamar al método correspondiente de la instancia de Repository para crear una nueva actividad
-    repository.createActivity(title, description, imgUrl);
-  
-    // Invocar la función para refrescar el contenedor de actividades
-    updateCardContainer();
-  
-    // Limpiar los campos del formulario después de agregar la actividad
-    titleInput.value = "";
-    descriptionInput.value = "";
-    urlInput.value = "";
-  }
-  
-  // Obtener el botón mediante su ID
-  const addActivityButton = document.getElementById("addActivity");
-  
-  // Agregar un evento de clic al botón
-  addActivityButton.addEventListener("click", handlerCreateActivity);
-  
-  // Función para manejar la eliminación de actividades
-  const handleDeleteActivity = (activityId) => {
-    // Llamar al método correspondiente de la instancia de Repository para eliminar la actividad
-    repository.deleteActivity(activityId);
-  
-    // Invocar la función para refrescar el contenedor de actividades
-    updateCardContainer();
-  }
-  
 
+    repository.createActivity(titleValue,descriptionValue,imgUrlValue)
 
+    builActivities()
 
+    clearFields()
+
+}
+
+const clearFields = () => {
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('imgUrl').value = '';
+};
+
+const buttonSubmit = document.getElementById("submitButton")
+buttonSubmit.addEventListener('click',handleClick);
 
 
 
